534f467f7dc4561ea7a3f38187a5f7427679e96a
gioMana1407 <g-manashvili@mail.ru>
2024-09-09 19:34:41 +0400

    second commit

---
+++ Day 01/homework/lesson1.py
@@ -0,0 +1,15 @@
+name = "giorgi"
+surname = "manashvili"
+age = 17
+height = 181.1
+is_human="true"
+knows_programing="false"
+
+print(name + " " + surname + " " ,str(age) + " " + str(height) )
+
+
+
+
+
+
+
