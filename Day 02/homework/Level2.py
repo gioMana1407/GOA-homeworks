b74bccbdbf131e98b786aa7a0ce5635345271fd8
gioMana1407 <g-manashvili@mail.ru>
2024-09-11 16:14:07 +0400

    second commit

---
+++ Day 02/homework/Level2.py
@@ -0,0 +1,13 @@
+name = "გიორგი"
+age = 17
+favorite_food = "ლობიანი"
+favorite_color = "მწვანე"
+
+print("მე","მქვია",name,"და","ვარ",age,"წლის")
+
+print("ჩემი","საყვარელი","საჭმელი","არის",favorite_food,)
+print("ჩემი","საყვარელი","ფერი","არის",favorite_color,)
+
+
+
+
