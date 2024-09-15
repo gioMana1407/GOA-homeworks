2def21297cd52847ced64d40e790c5a8efb64521
gioMana1407 <g-manashvili@mail.ru>
2024-09-15 16:53:01 +0400

    second commit

---
+++ Day 03/classwork/Day 03/classwork.py
@@ -0,0 +1,17 @@
+name = "giorgi"
+last_name = "manashvili"
+
+print(name + " " + last_name,)
+
+
+num1 = 100
+num2 = 150.1
+
+print(num1 + 100)
+print(num1 - 100)
+print(num1 * 100)
+print(num1 ** 100)
+print(num1 / 100)
+print(num1 // 100)
+
+print(num1 + num2)
---
+++ Day 03/homework/python.py
@@ -0,0 +1,38 @@
+name = "გიორგი"
+surname = "მანაშვილი"
+age = "17"
+hobby = "ფეღბურთის_თამაში"
+country = "ქართველი"
+favorite_food = "ლობიანი"
+favorite_color = "მწვანე"
+favorite_profession = "პროგრამირება"
+city = "თბილისი"
+favorite_movie = "lifes_is_beautiful"
+num1 = 10
+num2 = 5
+
+print("მე","მქვია",name,"და","ჩემი","გვარია",surname,"ვარ",age,"წლის","ჩემი","ჰობია",hobby,"ვარ",country,"საყვარელი","საჭმელი",favorite_food,"საყვარელი","ფერი",favorite_color,"საყვარელი","პროფესია",favorite_profession,"ქალაქი",city,"საყვარელი","ფილმი",favorite_movie)
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 ** num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
