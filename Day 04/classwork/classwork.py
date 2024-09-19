f0aa5e223b07eb6685dafd0f6e06dd14bf00fc0c
gioMana1407 <g-manashvili@mail.ru>
2024-09-19 16:09:56 +0400

    commit

---
+++ Day 04/classwork/classwork.py
@@ -0,0 +1,42 @@
+num1 = 10
+num2 = 15
+sum = 25
+print(sum)
+
+price = 50
+
+
+price = 60
+
+print(price)
+
+# debbuging არის შეცდომების შესწორება
+# Exercise 1:
+print("Hello World")
+
+# Exercise 2: 
+greeting = "Good_Morning"
+print(greeting)
+
+# Exercise 3: 
+first_name = "Alice"
+last_name = "Smith"
+print( first_name + last_name)
+
+# Exercise 4:
+number = 50
+print("number")
+
+# Exercise 5: 
+name = "George"
+print(name)
+
+# Exercise 6: 
+age = 25
+print("I am", age, "years old.")
+
+# Exercise 7: 
+num1 = 10
+num2 = 20
+result = num1 + num2
+print(result)
---
+++ Day 04/homework/lesson.py
@@ -0,0 +1,27 @@
+num1 = 5
+num2 = 10
+
+name = "გიორგი" 
+age = 17
+# num1 უდრის ცვლად 5ს ხოლო num2 10ს
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+print("მე","მქვია",name,"და","ვარ",str(age),"წლის")
+#name ცვლადი ასახავს გიორგის ხოლო age ცვლადი 17ს ფუნქცია სტრინგის დახმარებით 17 სტრინგად ვაქციე
+points = 20
+
+points1 = points + 2
+points2 = points + 5
+points3 = points + 10
+# ცვლადი points უდრის 20ს ცვლადი points1+points2+points3 საშუალებით ისინი 20ს დავუმატე და პრინტის საშვალებით გამოვიყვანე ტერმინალში
+print(points1)
+print(points2)
+print(points3)
+
+birthyear = 2007
+currentyear = 2024
+# ცვლადი birthyear უდრის ჩემი დაბადების წელს ხოლო ცვლადი currentyear ახლანდელ თარიღს პრინტის საშვალებით გამოვთვალე თუ რამდენი წელია გასული ჩემი დაბადების შემდეგ
+print(currentyear - birthyear)
