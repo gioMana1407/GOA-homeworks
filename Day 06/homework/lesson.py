c610a5d4506664930135e8180dc36933b3a5e82a
gioMana1407 <g-manashvili@mail.ru>
2024-09-26 17:21:43 +0400

    second commit

---
+++ Day 06/homework/lesson.py
@@ -0,0 +1,29 @@
+# name = (input("შენი დაბადების წელი"))
+
+# print(2024-int(name))
+
+# length = int(input("ოთხკუთედის სიგძე"))
+# width = int(input("ოთხკუთხედის სიგანე"))
+
+# print(length*width)
+# print(2*(length+width ))
+
+# distance = input("მანძილი სკოლამდე")
+
+# print(1500/100)
+# print(1500/10)
+# print(1500/1)
+# print(1500/0.1)
+# print(1500/1000)
+
+# name_surname = input("name surname")
+# parent_name_surname = input("parent name surname")
+# favorite_color = input("favorite color")
+# favorite_car = input("favorite car")
+# favorite_hobbies = input("favorite 3 hobbie")
+
+# num1 = int(input("რაიმე ორ ნიშნა რიცხვი"))
+# num2 = int(input("რიცხვი"))
+
+# print(num1 // num2)
+# print(num1 % num2)
