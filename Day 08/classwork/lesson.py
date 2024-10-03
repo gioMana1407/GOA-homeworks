026f828ffb35ef4d0422e06b5a4387129752dd27
gioMana1407 <g-manashvili@mail.ru>
2024-10-03 18:18:15 +0400

    second commit

---
+++ Day 08/classwork/lesson.py
@@ -0,0 +1,20 @@
+ 
+
+print(3 > 2)
+print(4 > 2)
+
+print(1 < 4)
+print(2 < 4)
+
+
+print(4 >= 4)
+print(5 >= 4)
+
+print(1 <= 1)
+print(1 <= 2)
+
+print(1 == 1)
+print(2 == 2)
+
+print(4 != 5)
+print(1 != 5)
---
+++ Day 08/homework/lesson.py
@@ -0,0 +1,73 @@
+# age = int(input("შენი ასაკი"))
+
+# print(age)
+# print(age > 13)
+# print(age < 19)
+# a = 10
+# b = (8 and 8)
+# c = (7 and 7)
+# d = (6 and 6)
+# f = 3
+
+
+# print(a >= 9)
+# print(b == 8)
+# print(c == 7)
+# print(d == 6)
+# print(f < 6)
+
+# true = 10 == 10
+# false = 3 < 2
+
+# print(true or true)
+# print(false or true)  
+# print(false or false)  
+# print(true or false)  
+# print(false and true)  
+# print(false and false)  
+# print(true and true)  
+# print(true and false)  
+
+# num1 = int(input("რაიმე რიცხვი"))
+# num2 = int(input("რაიმე რიცხვი"))
+
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 >= num2)
+# print(num1 <= num2)
+# print(num1 == num2)
+# print(num1 != num2)
+
+# a = 8
+# b = 11
+# c = 4
+
+# is_a_greatest = (8 > 11)
+# is_b_middle = (8 > 11 < 4)
+# is_c_least = (11 > 4)
+
+# print(is_a_greatest)
+# print(is_b_middle)
+# print(is_c_least)
+
+
+# score = 80
+# is_pass = (80 >= 50)
+# is_high_pass = (75 < 80 < 90)
+# is_perfect = (100 == 80)
+# is_failing = (50 > 80)
+
+# print(is_pass)
+# print(is_high_pass)
+# print(is_perfect)
+# print(is_failing)
+
+# p = 1 > 2
+# q = 2 > 1
+# P_and_not_Q = (p > q)
+# not_P_and_Q = (p < q)
+# P_xor_Q = (p or q)
+
+# print(P_and_not_Q)
+# print(not_P_and_Q)
+# print(P_xor_Q)
