fa4778a429c1ba5139fb494ec9a748fab177b1ad
gioMana1407 <g-manashvili@mail.ru>
2024-10-10 23:39:52 +0400

    second commit

---
+++ Day 10/classwork/lesson.py
@@ -0,0 +1,3 @@
+for iteration in range(1000):
+    print("goa" + str(iteration))
+    
---
+++ Day 10/homework/lesson.py
@@ -0,0 +1,39 @@
+# age = int(input("ასაკი"))
+
+# for iteration in range(100):
+#     print("age" + str(iteration))
+
+# temperature = 36
+# farengeit = 9/5 * temperature + 33.8 
+# print(farengeit)
+
+# num1 = 2
+# num2 = 3
+
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+
+# print(num1 < num2) 
+# print(num1 <= num2) 
+# print(num1 != num2) 
+
+loop1 = 10
+
+for loop in range(1,loop1 + 1):
+    print("*" * loop)
+
+
+
+
+
+
+
+
+
+
+# age = int(input("asaki"))
+# if age == 20:
+#     print(True)
+# if age != 20:
+#     print(False)
