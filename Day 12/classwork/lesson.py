cc4dd67d5e2244340a4bb4b82f7f2f4f0d3f64e8
gioMana1407 <g-manashvili@mail.ru>
2024-10-15 21:30:24 +0400

    second commit

---
+++ Day 12/classwork/lesson.py
@@ -0,0 +1,16 @@
+# for iteration in range(0,10000,2):
+#     print(iteration)
+
+
+# for iteration in range(1,10000,2):
+#     print(iteration
+
+i = 2 
+while i < 10:
+    print("umar!", i)
+    i += 1
+
+    i = 10
+while i > 10:
+    print("umar!", i)
+    i -= 1
