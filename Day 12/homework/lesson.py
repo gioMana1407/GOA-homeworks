4cdad5310b11afdd88c2f655fa86ba7135ab2ddd
gioMana1407 <g-manashvili@mail.ru>
2024-10-17 18:55:06 +0400

    second commit

---
+++ Day 12/homework/lesson.py
@@ -0,0 +1,38 @@
+# for i in range(11,50,2):
+#     print(i)
+
+# i = 1
+# while i < 10:
+#     print("GOA!",i)
+#     i += 1
+
+# while i > 0:
+#     print("GOA!",i)
+#     i -= 1
+
+# loop1 = 7
+
+# for loop in range(1,loop1,1):
+#     print("*******************")
+    
+    
+# loop2 = 5
+
+# for loop in range(1,loop2,1):
+#     print("*******************")
+
+# num1 = 5
+# num = 2
+
+# for i in range(10):
+#     num += 1
+#     for j in range(10):
+#         num1 +=1
+
+# print(num1 + num)
+
+user_password = input("type password: ")
+password = "GOA"
+while user_password != password:
+    user_password = input("inccorect try again:")
+print("login succsesfully")
