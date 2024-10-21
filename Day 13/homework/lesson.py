157117449609357c5791e594dbe02fcf11475082
gioMana1407 <g-manashvili@mail.ru>
2024-10-21 20:36:34 +0400

    second commit

---
+++ Day 13/homework/lesson.py
@@ -0,0 +1,11 @@
+for i in "name":
+    print(i)         
+# სიტყვა name-ს დამარცვლის როგორც n a m e
+
+
+    
+age = int(input("age"))
+if age > 10:
+    print("შენ ხარ 10 წლის ზემოთ")
+if age < 10:
+    print("false")
