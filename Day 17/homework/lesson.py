5cacd14d5d4b008939f88c19602b7a5cde6c83a2
gioMana1407 <g-manashvili@mail.ru>
2024-11-06 17:37:58 +0400

    second commit

---
+++ Day 17/homework/lesson.py
@@ -0,0 +1,19 @@
+                                  #boss level
+
+array = ["name","surname","age","tanki","lomi"]
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
+
+name = "hello"
+a = 0
+for i in name:
+    a += 1
+print(a)
+
+
+x = "GOA"
+print(x[::-1])
+
---
+++ Day 18/homework/lesson.py
@@ -0,0 +1,19 @@
+array = ["ბანანი","საზამთრო","მარწყვი","მსხალი"]
+print(array[0])
+print(array[-1])
+
+array = [0,1,2,3,4,5,6,7,8,9,10]
+print(array[0])
+print(array[5])
+print(array[-1])
+
+array = ["ძაღლი","კატა","სპილო","ნიანგი"]
+array[0] = "ლომი"
+print(array[0])
+
+array = ["life is beautiful","la haine","get out","schindler list"]
+print(array[-1])
+
+array = [10,20,30,40,50,60]
+print(array[-1])
+print(array[-2])
