a4b33103f39d17708910d40e40c4ee6e3e29b8dd
gioMana1407 <g-manashvili@mail.ru>
2024-11-11 19:42:39 +0400

    second commit

--- Day 17/homework/lesson.py
+++ Day 17/homework/lesson.py
@@ -1,19 +1,33 @@
                                   #boss level
 
-array = ["name","surname","age","tanki","lomi"]
-print(array[0])
-print(array[1])
-print(array[2])
-print(array[3])
-print(array[4])
+# array = ["name","surname","age","tanki","lomi"]
+# print(array[0])
+# print(array[1])
+# print(array[2])
+# print(array[3])
+# print(array[4])
 
-name = "hello"
-a = 0
-for i in name:
-    a += 1
-print(a)
+# name = "hello"
+# a = 0
+# for i in name:
+#     a += 1
+# print(a)
 
+# array = ["davit", "gio", "davit", "gio", "davit"]
+# b = 0
+# for i in array:
+#     if i == "davit":
+#         b += 1
+# print(b)
 
-x = "GOA"
-print(x[::-1])
+# sia = [5, 10, 15, 20]
+# sum = 0
+# for i in sia:
+#     sum += i
+# print(sum / len(sia))
+
+#შევქმნით ცვლადს რომელიც თავიდან = 0 ფორ ციკლით გადავუვლით სიას შემდეგ თითოეულ ელემენტს შევკრიბავთ 
+
+# x = "GOA"
+# print(x[::-1])
 
