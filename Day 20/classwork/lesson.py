f68735ee738c278b33e62249a3f0d99031e1a5bd
gioMana1407 <g-manashvili@mail.ru>
2024-11-13 19:05:14 +0400

    second commit

---
+++ Day 20/classwork/lesson.py
@@ -0,0 +1,6 @@
+num = [1,2,3,4,5,6,7,8]
+for i in range(len(num)):
+    print(num[i])
+    if num[i] % 2 == 0:
+        print("kenti")
+        
---
+++ Day 20/homework/lesson.py
@@ -0,0 +1,32 @@
+# array = ["ა","ბ","გ","დ","ე","ვ","ზ","თ","ი","კ","ლ","მ","ნ"]
+# for i in array:
+#     if  i in("ა","ე","ი","ო","უ" ):
+#      print(i)
+
+# array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# for i in range(len(array)):
+#     if array[i] % 5 == 0 and array[i] % 3 == 0:
+#         print(array[i])
+
+
+# array = ["1","a","2","goa","gio","17"]
+# sting = ""
+# for i in array:
+#     sting += i
+
+# print(sting)
+
+
+# array = [" ******", "  ******", "   ******", "    ******"]
+
+# print(array[0])
+# print(array[1])
+# print(array[2])
+# print(array[3])
+
+
+# age = int(input("age"))
+# if age >= 12:
+#     print("you are 12 years old or more")
+# else:
+#     print("you are not 12 years old")
