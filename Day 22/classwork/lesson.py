1b86e831c9e0d302b7bd0dda95104f23eee93bfa
gioMana1407 <g-manashvili@mail.ru>
2024-11-21 18:00:07 +0400

    second commit

---
+++ Day 22/classwork/lesson.py
@@ -0,0 +1,8 @@
+even_numbers = []
+odd_numbers = []
+
+for i in range(1, 1001):
+    if i % 2 == 0:
+        even_numbers.append(i)
+    else:
+        even_numbers.append(i)
---
+++ Day 22/homework/lesson.py
@@ -0,0 +1,24 @@
+list_of_names = ["gio","luka","davit","davit","nika"]
+counter = 0
+for i in list_of_names:
+    if i == "davit":
+        counter += 1
+print(counter)
+
+list = [1,2,3,4,5,6]
+list.reverse()
+print(list)
+
+arr = ["1","2","3"]
+arr1 = (arr * 3)
+print(arr1)
+
+array = ["a",'b',"c","e"]
+array.insert(-1,"davit")
+print(array)
+
+arra = ["1","2","1","3","1"]
+arrayy = arra.count("2")
+arra.remove("2")
+print(arra)
+print(arrayy)
