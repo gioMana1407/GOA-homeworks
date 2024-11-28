08e70eaa6f46151dfb8ee8185ecc3be9baabc329
gioMana1407 <g-manashvili@mail.ru>
2024-11-28 19:16:45 +0400

    second commit

---
+++ Day 24/classwork/lesson.py
@@ -0,0 +1,9 @@
+message = "წითელი ვაშლი გაგორდა {0}".format("50ml")
+print(message)
+
+name = "იხვის ტოლმა არ არის ბატის ტოლმა"
+split_name = name.split()
+join_name = " ".join(split_name)
+
+print(split_name)
+print(join_name)
