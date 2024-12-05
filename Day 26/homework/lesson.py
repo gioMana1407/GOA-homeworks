391037c6f31f5d5913f3058ceeef4ff56271951c
gioMana1407 <g-manashvili@mail.ru>
2024-12-05 18:29:27 +0400

    second commit

---
+++ Day 26/homework/lesson.py
@@ -0,0 +1,81 @@
+from turtle import *
+
+def triangle1(triangle12):
+    for i in range(triangle12):
+        pencolor("blue")
+        forward(240)
+        left(120)
+        forward(210)
+        left(115)
+        forward(225)
+        speed(100)
+
+def triangle2(triangle21):
+    for i in range(triangle21):
+        pencolor("green")
+        forward(240)
+        left(120)
+        forward(210)
+        left(115)
+        forward(225)
+        speed(100)
+
+triangle1(60)
+triangle2(60)
+
+
+exitonclick()
+
+
+
+
+
+
+# def hello_world():
+#     print("print")
+
+# hello_world()
+    
+
+# def even_od_odd():
+#     num = int(input("raime ricxvi: "))
+#     print(f"ricxvi luwia :)" if num % 2 == 0 else "ricxvi kentia :)" )
+
+# even_od_odd()
+
+# def square(square1):
+#     for i in range(square1):
+#         arr = ["******","******","******"]
+#         print(arr[0])
+#         print(arr[1])
+#         print(arr[2])
+    
+
+# square(120)
+
+
+
+# def square(tree1):
+#     for i in range(tree1):
+#         arr = ["     *","    ***","  *******","***********","     *","     *"]
+#         print(arr[0])
+#         print(arr[1])
+#         print(arr[2])
+#         print(arr[3])
+#         print(arr[4])
+#         print(arr[5])
+    
+
+# square(120)
+
+
+# def square(12):
+#     for i in range(12):
+#         arr = ["*******"," *******","  *******","   *******"]
+#         print(arr[0])
+#         print(arr[1])
+#         print(arr[2])
+#         print(arr[3])
+    
+
+# square(120)
