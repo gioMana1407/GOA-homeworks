a5946adef1e3fc202c10055d47ee08d70150323d
giomana1407 <g-manashvili@mail.ru>
2025-01-04 16:52:13 +0400

    second commit

---
+++ Day 34/classwork/lesson.py
@@ -0,0 +1,7 @@
+tuflebi = (1,2,3,4,5,6)
+first , *rest = tuflebi
+
+print(first)
+print(rest)
+print(tuflebi[0])
+print(tuflebi[-1])
---
+++ Day 34/homework/lesson.py
@@ -0,0 +1,16 @@
+tuflebi = (1,2,3,4,5)
+print(tuflebi[0])
+print(tuflebi[1])
+
+numbers = (2, 56, 99, 22, 15, 23, 66, 11, 134, 23, 66, 91, 22, 2, 23)
+print(numbers.count(23))
+
+aura = (10, 25, 5, 80, 70, 20)
+for i in aura:
+    if i > 10:
+        print(i)
+    
+# listi aris sia romlis shecvlac chven shegvilia xolo tuplebshi chven ar shegvilia elementi shevcvalot
+
+#ummutable = tuple
+#mutable - list
